a1f26720456d33d6ac12813b46abc351bf9b0881
Adam Florczak <adam.florczak@me.com>
2016-11-16 21:27:05 +0100

    Feedback meta component

--- client/src/feedback-list/comment/comment.tsx
+++ client/src/feedback-list/comment/comment.tsx
@@ -1,15 +1,19 @@
 import * as React from 'react';
+import * as moment from 'moment';
 
 import './comment.scss';
+import { FeedbackMeta } from '../feedback-meta/feedback-meta';
 
 export class Comment extends React.Component<{}, {}> {
   render() {
     return (
        <div className='cr-comment'>
+         <FeedbackMeta name='Maria'
+                       date={moment().subtract(10, 'days')} />
          <div className='cr-comment__content'>
            Some content
          </div>
        </div>
-    )
+    );
   }
 }
